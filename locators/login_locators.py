439c3be32bf022be14b2a74f9c99411bc0761a5e
Dilyara Runova <dilyararunova@yandex.ru>
2026-02-09 20:41:46 +0300

    Очистка ветки main, оставлен только README.md

--- locators/login_locators.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LoginPageLocators:
-    EMAIL_INPUT = (By.XPATH, "//input[@type='text' and @name='name']")
-    PASSWORD_INPUT = (By.XPATH, "//input[@type='password']")
-    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
-    REGISTER_LINK = (By.XPATH, "//a[text()='Зарегистрироваться']")
-    FORGOT_PASSWORD_LINK = (By.XPATH, "//a[text()='Восстановить пароль']")
